aa6c7079cb04e7fda8426bccc0268e52ffb8a1f8
yuzokamoto <yuzokamoto@gmail.com>
2020-09-24 17:47:43 -0300

    Preparing code for upload feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import morgan from "morgan";
 import { AddressInfo } from "net";
 import { userRouter } from "./router/userRouter";
 import { pinRouter } from "./router/pinRouter";
@@ -9,6 +10,8 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 app.use(cors());
 
 app.use("/user", userRouter);
